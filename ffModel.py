d3624918b48385c55cd5e26cfba71b2023581f0b
Jacob Levine <165849895+AbogAscended@users.noreply.github.com>
2024-06-26 15:32:07 -0700

    Basic Feedforward implementation using ReLU neurons and MSE loss function for univariate regression. This was an implementation using the dont overfit 2 kaggle competition dataset.

--- ffModel.py
+++ ffModel.py
@@ -1,15 +1,9 @@
 import torch.nn as nn
-
-
 class FeedForward(nn.Module):
     def __init__(self):
         super().__init__()
         self.sequential = nn.Sequential(
             nn.Linear(300, 50),
-            nn.ReLU(),
-            nn.Linear(50, 50),
-            nn.ReLU(),
-            nn.Linear(50, 50),
             nn.ReLU(),
             nn.Linear(50, 50),
             nn.ReLU(),
