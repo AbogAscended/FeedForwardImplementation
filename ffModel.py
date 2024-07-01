d41a79edb2c43b2a45a9bd48a1aac1362deef238
Jacob Levine <165849895+AbogAscended@users.noreply.github.com>
2024-07-01 13:50:03 -0700

    Basic Feedforward implementation using ReLU neurons and MSE loss function for univariate regression. This was an implementation using the dont overfit 2 kaggle competition dataset.

--- ffModel.py
+++ ffModel.py
@@ -1,14 +1,38 @@
 import torch.nn as nn
+import torch.nn.init as init
+
+
 class FeedForward(nn.Module):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, dropout_rate):
+        super(FeedForward, self).__init__()
         self.sequential = nn.Sequential(
-            nn.Linear(300, 50),
-            nn.ReLU(),
-            nn.Linear(50, 50),
-            nn.ReLU(),
-            nn.Linear(50, 1)
+            nn.Linear(287, 500),
+            nn.ELU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(500, 500),
+            nn.ELU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(500, 500),
+            nn.ELU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(500, 500),
+            nn.ELU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(500, 500),
+            nn.ELU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(500, 500),
+            nn.ELU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(500, 1)
         )
 
     def forward(self, x):
         return self.sequential(x)
+
+    def init_weights(self):
+        for layer in self.sequential:
+            if isinstance(layer, nn.Linear):
+                init.kaiming_uniform_(layer.weight, nonlinearity='relu')
+                if layer.bias is not None:
+                    init.constant_(layer.bias, 0)
